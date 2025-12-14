a53fc078bed2c951f09648ddc443f949c744e0ed
rbespada <rbuendiaespada@gmail.com>
2025-12-14 07:02:05 +0100

    Ejercicio 1 completado

---
+++ ejercicios-basicos-avanzados/ejercicio-01.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Ejercicio 01
+// 1) miSuperheroeFavorito = "Hulk"
+// 2) numeroFavorito = 50
+// 3) altura = 5, longitud = 10
+// 4) suma = altura + longitud
+// 5) mostrar TODO por consola
+
+let miSuperheroeFavorito = "Hulk";
+let numeroFavorito = 50;
+let altura = 5;
+let longitud = 10;
+let suma = altura + longitud;
+
+console.log(miSuperheroeFavorito);
+console.log(numeroFavorito);
+console.log(altura);
+console.log(longitud);
+console.log(suma);
