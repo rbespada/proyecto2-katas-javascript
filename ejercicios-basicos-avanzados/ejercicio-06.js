074847c46071c715fef2f9c1d6babd61e4370ada
rbespada <rbuendiaespada@gmail.com>
2025-12-15 19:21:50 +0100

    Ejercicio-06 y 07 .js  Hechos

---
+++ ejercicios-basicos-avanzados/ejercicio-06.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Ejercicio 06
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+
+for (let i = 0; i < 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 10) {
+    console.log("¡Dormido!");
+  } else {
+    console.log("Intentando dormir");
+  }
+}
+
+
---
+++ ejercicios-basicos-avanzados/ejercicio-07.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Ejercicio 07
+
+function greaterNumber(numberOne, numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log(numberOne);
+    } else {
+        console.log(numberTwo);
+    }
+}
+
+greaterNumber(10, 5);
+greaterNumber(3, 7);
+greaterNumber(20, 20);
