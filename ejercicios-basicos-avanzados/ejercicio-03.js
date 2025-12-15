ed10926178aacd48df3a6dcf68d68472a0d95dd1
rbespada <rbuendiaespada@gmail.com>
2025-12-15 18:29:06 +0100

    Ejercicio-03.js hecho

---
+++ ejercicios-basicos-avanzados/ejercicio-03.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Ejercicio 03
+
+console.log(10 * 5);
+console.log(10 / 2);
+console.log(15 % 9);
+
+
+let p = 10;
+let j = 5;
+let o = p += j;
+console.log(o);
+
+
+let c = 10;
+let m = 5;
+
+let i = c *= m;
+console.log(i);
+
